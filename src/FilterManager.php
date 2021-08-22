3760244c7097b0558bad9016b6d2e226ae45a530
Puleeno Nguyen <puleeno@gmail.com>
2021-08-22 09:14:20 +0700

    Update simple filter stylesheet

--- src/FilterManager.php
+++ src/FilterManager.php
@@ -5,7 +5,7 @@ use Jankx\Filter\Filters\SimpleFilter;
 
 class FilterManager
 {
-    const VERSION = '1.0.0.4';
+    const VERSION = '1.0.0.10';
 
     protected $filterStyles;
     protected $rootDirUrl;
