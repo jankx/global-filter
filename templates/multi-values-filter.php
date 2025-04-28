bbf76efedfd61e9286fac39527696c3b35063eef
Puleeno Nguyen <puleeno@gmail.com>
2025-04-28 14:23:20 +0700

    fix error templates

--- templates/multi-values-filter.php
+++ templates/multi-values-filter.php
@@ -1,17 +1,15 @@
-<form class="filter-options">
-    <?php foreach($filter_options as $option): ?>
-        <div class="filter-option">
-            <label for="<?php echo $filter_type; ?>-<?php echo $option->getDataType(); ?>-<?php echo $option->getId(); ?>">
-                <input
-                    type="checkbox"
-                    class="hidden filter-control"
-                    id="<?php echo $filter_type; ?>-<?php echo $option->getDataType(); ?>-<?php echo $option->getId(); ?>"
-                    name="<?php echo esc_attr($data_type); ?>[<?php echo esc_attr($option->getDataType()); ?>][]"
-                    value="<?php echo esc_attr($option->getId()); ?>"
-                />
-                <div class="virtual-checkbox"></div>
-                <?php echo $option->getName(); ?>
-            </label>
-        </div>
-    <?php endforeach; ?>
-</form>
+<?php foreach($filter_options as $option): ?>
+    <div class="filter-option">
+        <label for="<?php echo $filter_type; ?>-<?php echo $option->getDataType(); ?>-<?php echo $option->getId(); ?>">
+            <input
+                type="checkbox"
+                class="hidden filter-control"
+                id="<?php echo $filter_type; ?>-<?php echo $option->getDataType(); ?>-<?php echo $option->getId(); ?>"
+                name="<?php echo esc_attr($data_type); ?>[<?php echo esc_attr($option->getDataType()); ?>][]"
+                value="<?php echo esc_attr($option->getId()); ?>"
+            />
+            <div class="virtual-checkbox"></div>
+            <?php echo $option->getName(); ?>
+        </label>
+    </div>
+<?php endforeach; ?>
